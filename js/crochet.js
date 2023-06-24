85d08a9e68418ca8061070605e3b78f899b9eb78
unknown <sakrsalma42@gmail.com>
2023-06-24 18:35:46 +0300

    second commit

--- js/crochet.js
+++ js/crochet.js
@@ -1,13 +1,15 @@
 
-  const reviewstars=document.querySelectorAll(".review i");
-  reviewstars.forEach((star,index)=>{
-    star.addEventListener("click",()=>{
-        reviewstars.forEach((el)=>el.classList.remove("fas","checked"));
-       for(let i=0;i<=index;i++)
-       {
-        reviewstars[i].classList.add("fas","checked");
-       }
-    const starcount=document.querySelectorAll(".review i.checked").length;
-    console.log(starcount);
-    });
+
+
+const reviewstars = document.querySelectorAll(".review i");
+reviewstars.forEach((star, index) => {
+  star.addEventListener("click", () => {
+    var stars = star.parentNode.querySelectorAll("i");
+    stars.forEach((el) => el.classList.remove("fas", "checked"));
+    var indexOfStar = Array.prototype.slice.call(stars).indexOf(star);
+
+    for (let i = 0; i <= indexOfStar ; i++) {
+      stars[i].classList.add("fas", "checked");
+    }
   });
+});
